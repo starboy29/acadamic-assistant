84cc595ae0e9fb9d8c39021aed45e070f8b304dd
starboy29 <111701199+starboy29@users.noreply.github.com>
2025-07-07 00:45:23 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-GUILD_ID = int(os.getenv("GUILD_ID"))
-CALENDER_ID=os.getenv("CALENDER_ID")
